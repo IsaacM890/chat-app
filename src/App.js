838c556f5a1b2d5b882935bc74515ff391d5570b
IsaacM890 <62258398+IsaacM890@users.noreply.github.com>
2021-06-12 20:11:55 +0300

    adding dependencies + initialize chat engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+/** @format */
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height='100vh'
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName='itzhak123'
+      userSecret={process.env.REACT_APP_USER_SECRET}
+    />
   );
-}
+};
 
 export default App;
