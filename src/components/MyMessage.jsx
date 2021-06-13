01b3d1cc4709b3ae70fc0d084394befde0d45bdd
IsaacM890 <62258398+IsaacM890@users.noreply.github.com>
2021-06-13 20:29:20 +0300

    adding my message comp

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,30 @@
 /** @format */
 
-const MyMessage = () => {
-  return <div>MyMessage</div>;
+const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        className='message-image'
+        style={{ float: 'right' }}
+        src={message.attachments[0].file}
+        alt='message-attachments'
+      />
+    );
+  }
+
+  return (
+    <div
+      className='message'
+      style={{
+        float: 'right',
+        marginRight: '18px',
+        color: 'white',
+        backgroundColor: '#3B2A50',
+      }}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default MyMessage;
